2a206c734f8cc187d330f3cd9e94fbaf1481fbbb
Juan <navarrojgr@gmail.com>
2025-01-15 22:07:55 -0600

    gh pages fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/typing-speed-app.git/",
+  base: "./typing-speed-app.git/",
   plugins: [react()],
 })
